87529c59f2c3de76c65cd8ed7606495a176a246c
seamly01 <57246513+seamly01@users.noreply.github.com>
2019-11-24 22:11:20 +0800

    Update ID.js

--- ID.js
+++ ID.js
@@ -3,6 +3,6 @@
 */
 
 var body = $response.body;
-body = '\/*\n@supported 28D0150923AA, CF1FE6F0B3BC\n*\/\n' + body;
+body = '\/*\n@supported 28D0150923AA CF1FE6F0B3BC\n*\/\n' + body;
 
 $done(body);
